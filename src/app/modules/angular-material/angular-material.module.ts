ff57cb10c5a6ec568aefcecfa81b8216e46da503
satish <satishkonduru0225@gmail.com>
2025-06-21 12:08:48 +0530

    Setup

---
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+const materailComponents = [];
+@NgModule({
+  imports: [materailComponents],
+  exports: [materailComponents],
+})
+export class AngularMaterialModule {}
