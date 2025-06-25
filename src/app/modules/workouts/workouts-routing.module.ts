3487d1ae295e38e5c77f79fc1c91ade249438f20
satish <satishkonduru0225@gmail.com>
2025-06-25 18:53:44 +0530

    Workouts module with sidenav

--- src/app/modules/workouts/workouts-routing.module.ts
+++ src/app/modules/workouts/workouts-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkoutsComponent } from './workouts.component';
 import { NewWorkoutComponent } from './new-workout/new-workout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
@@ -9,12 +10,20 @@ const routes: Routes = [
     component: WorkoutsComponent,
     children: [
       {
+        path: '',
+        component: DashboardComponent, // shows by default inside layout
+      },
+      {
         path: 'new',
         component: NewWorkoutComponent,
       },
+      // more routes can go here
     ],
   },
+  // optional: add fallback route
+  { path: '**', redirectTo: '' },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/workouts/workouts.module.ts
+++ src/app/modules/workouts/workouts.module.ts
@@ -5,9 +5,10 @@ import { WorkoutsRoutingModule } from './workouts-routing.module';
 import { WorkoutsComponent } from './workouts.component';
 import { NewWorkoutComponent } from './new-workout/new-workout.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [WorkoutsComponent, NewWorkoutComponent],
+  declarations: [WorkoutsComponent, NewWorkoutComponent, DashboardComponent],
   imports: [CommonModule, WorkoutsRoutingModule, AngularMaterialModule],
 })
 export class WorkoutsModule {}
