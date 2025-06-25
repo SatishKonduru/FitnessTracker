0b5d63c3ded7489df504ae77c309abeadb82e109
satish <satishkonduru0225@gmail.com>
2025-06-25 11:54:45 +0530

    Working on SignUp

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  today = new Date();
+  maxDOB: Date = new Date(
+    new Date().setFullYear(new Date().getFullYear() - 18)
+  );
+  minDOB: Date = new Date(1900, 0, 1); // Jan 1, 1900
+  dob: Date | null = null;
 
+  get age(): number | null {
+    if (!this.dob) return null;
+
+    const today = new Date();
+    let age = today.getFullYear() - this.dob.getFullYear();
+    const m = today.getMonth() - this.dob.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < this.dob.getDate())) {
+      age--; // birthday hasn't occurred yet this year
+    }
+
+    return age;
+  }
+
+  onSubmit = (form: NgForm) => {
+    console.log('Form Data: ', form.value);
+  };
 }
--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -4,12 +4,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 const materailComponents = [
   MatToolbarModule,
   MatFormFieldModule,
   MatInputModule,
   MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
 ];
 @NgModule({
   imports: [materailComponents],
